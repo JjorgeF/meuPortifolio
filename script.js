76ecb50f0fc259e8901b3be696fe7cc126bb0e0e
Jorge Felipe <jorgefelipe0299@gmail.com>
2023-04-07 21:42:36 -0300

    alterando o js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
-})
+
 window.alert("Atenção... Ainda está em construção, caso encontre erros, avise-me por favor!");
+})
