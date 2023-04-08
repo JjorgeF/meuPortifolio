d051ce4be6daa68840c74f64abd57cf6402cc98f
Jorge Felipe <jorgefelipe0299@gmail.com>
2023-04-07 21:40:20 -0300

    alert através do js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
 })
+window.alert("Atenção... Ainda está em construção, caso encontre erros, avise-me por favor!");
